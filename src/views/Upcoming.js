a12bbf52bdf2ca640c38fba13363bdef7f8f88ab
James Batt <place.net.au@gmail.com>
2016-04-27 05:48:14 -0700

    added layout

--- src/views/Upcoming.js
+++ src/views/Upcoming.js
@@ -3,30 +3,36 @@ import React, {PropTypes} from 'react';
 class Upcoming extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
-		this.state.fakeData = [{
-			"id": 1,
-			"title": "Wired Monash UNIHACK 2016",
-			"description": "lorem ipsum lorem ipsum lorem ipsum",
-			"thumbnail": "https://placeholdit.imgix.net/~text?txtsize=33&txt=150%C3%97150&w=150&h=150"
-		}, {
-			"id": 2,
-			"title": "Wired Monash UNIHACK 2016",
-			"description": "lorem ipsum lorem ipsum lorem ipsum",
-			"thumbnail": "https://placeholdit.imgix.net/~text?txtsize=33&txt=150%C3%97150&w=150&h=150"
-		}];
+		this.state = {
+			events: []
+		};
+	}
+
+	componentDidMount() {
+		// request real data from UV api
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', 'http://localhost:8000/api/v0.2/event/trending/', true);
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState == XMLHttpRequest.DONE) {
+				this.setState({
+					events: JSON.parse(xhr.responseText).results
+				});
+			}
+		};
+		xhr.setRequestHeader("Authorization", "Token 4a8ca0a0436c4b89e99e6cb97bfcc5b15b2888b0");  // testing key only
+		xhr.send(null);
 	}
 
 	render() {
 		return (
-			<div>
-				{this.state.fakeData.map(data => {
+			<div className="list">
+				{this.state.events.map(data => {
 					return (
 						<div className="listItem" key={data.id}>
-							<img className="thumbnail" src={data.thumbnail} />
+							<img className="thumbnail" src={data.facebookProfileSource} />
 							<div className="content">
-								<h2 className="title">{data.title}</h2>
-								<p className="description">{data.description}</p>
+								<h2 className="title">{data.name}</h2>
+								<p className="description">{data.description.substring(0, Math.min(150, data.description.length))}</p>
 							</div>
 						</div>
 					);
