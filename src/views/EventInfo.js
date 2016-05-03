14263a35cd5fac653d93d6e1ffd0d7370fd5d4a2
James Batt <place.net.au@gmail.com>
2016-05-03 13:22:16 +1000

    added more styles for event info page

--- src/views/EventInfo.js
+++ src/views/EventInfo.js
@@ -1,19 +1,46 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
+function mapStateToProps(state, props) {
+	return {
+		event: state.events.find(element => {
+			if (element.id == props.params.id) {
+				return element;
+			}
+		})
+	};
+}
+
+@connect(mapStateToProps, null)
 class EventInfo extends React.Component {
 
 	static propTypes = {
-
-	}
-
-	constructor(props) {
-		super(props);
+		event: PropTypes.object.isRequired,
 	}
 
 	render() {
 		return (
-			<div>
-				EventInfo
+			<div className="eventInfo">
+				<img className="eventCoverImage" src={this.props.event.facebookCoverSource} />
+				<div className="eventStatsBox">
+					<div className="eventStatsBoxItem">
+						<div>{this.props.event.attendingCount}</div>
+						<div>Attending</div>
+					</div>
+					<div className="eventStatsBoxItem">
+						<div>{this.props.event.maybeCount}</div>
+						<div>Maybe</div>
+					</div>
+					<div className="eventStatsBoxItem">
+						<div>{this.props.event.attendingCount + this.props.event.maybeCount + this.props.event.noReplyCount}</div>
+						<div>Invited</div>
+					</div>
+				</div>
+				<div className="eventContent">
+					<h2 className="eventTitle">{this.props.event.name}</h2>
+					<span className="eventDate">{new Date(this.props.event.startTime).toDateString()}</span>
+					<p className="eventDescription">{this.props.event.description}</p>
+				</div>
 			</div>
 		);
 	}
