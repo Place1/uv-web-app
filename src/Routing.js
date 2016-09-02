2198c8cd6bcf17a417daaf2af70a6ee8a19314ae
James Batt <place.net.au@gmail.com>
2016-09-02 14:29:39 +1000

    updated Routing to be a pure functional component

--- src/Routing.js
+++ src/Routing.js
@@ -3,17 +3,15 @@ import App from './App';
 import * as views from './views';
 import { Router, IndexRoute, Route, hashHistory } from 'react-router';
 
-const Routing = React.createClass({
-	render: function() {
-		return (
-			<Router history={hashHistory}>
-				<Route path="/" component={App}>
-					<IndexRoute component={views.Upcoming} />
-					<Route path="/event/:id" component={views.EventInfo} />
-				</Route>
-			</Router>
-		)
-	}
-});
+function Routing() {
+	return (
+		<Router history={hashHistory}>
+			<Route path="/" component={App}>
+				<IndexRoute component={views.Upcoming} />
+				<Route path="/event/:id" component={views.EventInfo} />
+			</Route>
+		</Router>
+	)
+}
 
 export default Routing;
